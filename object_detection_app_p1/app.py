e123644aa363c59eb3070bbae4212c8cbbad5ece
Sohini Roychowdhury <50978513+sohiniroych@users.noreply.github.com>
2021-09-14 14:45:11 -0700

    Update app.py

--- object_detection_app_p1/app.py
+++ object_detection_app_p1/app.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz'
+MODEL_URL = 'http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet50_coco_2018_01_28.tar.gz'
 
 import base64
 import io
